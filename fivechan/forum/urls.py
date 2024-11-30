d7443a7d15f9e1545e1a105f09670662de081133
misha56803 <misha56803@gmail.com>
2024-11-30 18:43:52 +0300

    Запутался с директориями

--- fivechan/forum/urls.py
+++ fivechan/forum/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
@@ -7,6 +8,9 @@ urlpatterns = [
     path('create/', views.create_thread, name='create_thread'),
 ]
 
+# Аутентификация
 urlpatterns += [
-    path('accounts/register/', views.register, name='register'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/register/', auth_views.LoginView.as_view(template_name='registration/register.html'), name='register'),
 ]
