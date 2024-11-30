fe03310dd126ba9701f49efe8d6fa28be366b486
misha56803 <misha56803@gmail.com>
2024-11-30 22:59:43 +0300

    Улучшил визуальную составляющую

--- fivechan/fivechan/settings.py
+++ fivechan/fivechan/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Настройки статических файлов
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Папка для пользовательских статических файлов
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
