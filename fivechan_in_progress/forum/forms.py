d5157e91b0f0dc2c7d43151a91136badff16db7c
misha56803 <misha56803@gmail.com>
2024-12-09 23:52:23 +0300

    выкидывает ошибку при попытки регистрации с неправльного адреса почты

--- fivechan_in_progress/forum/forms.py
+++ fivechan_in_progress/forum/forms.py
@@ -15,15 +15,19 @@ class CommentForm(forms.ModelForm):
 
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True,  help_text="Введите почту с доменом @edu.hse.ru")
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
     def save(self, commit=True):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("@edu.hse.ru"):
+            raise forms.ValidationError("Регистрация доступна только с адресами @edu.hse.ru.")
+
         user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
+        user.email = email
         user.is_active = False  # Деактивируем пользователя до подтверждения
         if commit:
             user.save()
